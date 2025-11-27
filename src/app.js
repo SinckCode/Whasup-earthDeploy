6b4e78343079dae92fe9ad6fd332bf14e0380b14
SinckCode <soyangeldavid1@gmail.com>
2025-11-27 15:01:20 -0600

    first co

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 // src/app.js
 require('dotenv').config();
 const express = require('express');
-const app = express();
-const routes = require('./routes');
+const { deploy } = require('./controllers/mainController'); // 👈 importar controller directo
 
+const app = express();
 app.use(express.json());
-app.use('/', routes);
+
+// Ruta única: POST /deploy
+app.post('/deploy', (req, res) => {
+  console.log('🔥 Entró a POST /deploy');
+  deploy(req, res);
+});
 
 const PORT = process.env.PORT || 4000;
 
